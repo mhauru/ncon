fdcc365e092512290bfa60915f182597cb0f96ab
Markus Hauru <markus@mhauru.org>
2020-02-04 17:15:51 +0100

    Black and better style.

--- src/ncon/ncon.py
+++ src/ncon/ncon.py
@@ -1,8 +1,8 @@
+""" A module for the function ncon, which does contractions of several tensors.
+"""
 import numpy as np
 from collections import Iterable
 
-""" A module for the function ncon, which does contractions of several tensors.
-"""
 
 def ncon(AA, v, order=None, forder=None, check_indices=True):
     """ AA = [A1, A2, ..., Ap] list of tensors.
@@ -39,9 +39,9 @@ def ncon(AA, v, order=None, forder=None, check_indices=True):
     else:
         v = list(map(list, v))
 
-    if order == None:
+    if order is None:
         order = create_order(v)
-    if forder == None:
+    if forder is None:
         forder = create_forder(v)
 
     if check_indices:
@@ -53,9 +53,9 @@ def ncon(AA, v, order=None, forder=None, check_indices=True):
     connect_graph(AA, v, order)
 
     while len(order) > 0:
-        tcon = get_tcon(v, order[0]) # tcon = tensors to be contracted
+        tcon = get_tcon(v, order[0])  # tcon = tensors to be contracted
         # Find the indices icon that are to be contracted.
-        if len(tcon)==1:
+        if len(tcon) == 1:
             tracing = True
             icon = [order[0]]
         else:
@@ -63,7 +63,7 @@ def ncon(AA, v, order=None, forder=None, check_indices=True):
             icon = get_icon(v, tcon)
         # Position in tcon[0] and tcon[1] of indices to be contracted.
         # In the case of trace, pos2 = []
-        pos1, pos2 = get_pos(v, tcon, icon) 
+        pos1, pos2 = get_pos(v, tcon, icon)
         if tracing:
             # Trace on a tensor
             new_A = trace(AA[tcon[0]], axis1=pos1[0], axis2=pos1[1])
@@ -85,12 +85,14 @@ def ncon(AA, v, order=None, forder=None, check_indices=True):
     A = permute_final(A, vlast, forder)
     return A
 
+
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 
 def create_order(v):
     """ Identify all unique, positive indices and return them sorted. """
     flat_v = sum(v, [])
-    x = [i for i in flat_v if i>0]
+    x = [i for i in flat_v if i > 0]
     # Converting to a set and back removes duplicates
     x = list(set(x))
     return sorted(x)
@@ -101,7 +103,7 @@ def create_forder(v):
     (-1 first).
     """
     flat_v = sum(v, [])
-    x = [i for i in flat_v if i<0]
+    x = [i for i in flat_v if i < 0]
     # Converting to a set and back removes duplicates
     x = list(set(x))
     return sorted(x, reverse=True)
@@ -130,7 +132,9 @@ def connect_graph(AA, v, order):
             visited.add(i)
             # Get the indices of tensors neighbouring AA[i].
             i_inds = set(v[i])
-            neighs = (j for j, j_inds in enumerate(v) if i_inds.intersection(j_inds))
+            neighs = (
+                j for j, j_inds in enumerate(v) if i_inds.intersection(j_inds)
+            )
             for neigh in neighs:
                 if neigh not in visited:
                     to_visit.add(neigh)
@@ -155,7 +159,7 @@ def connect_graph(AA, v, order):
         except AttributeError:
             AA[d] = np.expand_dims(A_d, d_axis)
         try:
-            dim_num = max(order)+1
+            dim_num = max(order) + 1
         except ValueError:
             dim_num = 1
         v[c].append(dim_num)
@@ -176,19 +180,24 @@ def get_tcon(v, index):
     # If check_indices is called and it does its work properly then these
     # checks should in fact be unnecessary.
     if l > 2:
-        raise ValueError('In ncon.get_tcon, more than two tensors share a '
-                'contraction index.')
+        raise ValueError(
+            "In ncon.get_tcon, more than two tensors share a contraction "
+            "index."
+        )
     elif l < 1:
-        raise ValueError('In ncon.get_tcon, less than one tensor share a '
-                'contraction index.')
+        raise ValueError(
+            "In ncon.get_tcon, less than one tensor share a contraction index."
+        )
     elif l == 1:
         # The contraction is a trace.
         how_many = v[tcon[0]].count(index)
         if how_many != 2:
             # Only one tensor has this index but it is not a trace because it
             # does not occur twice for that tensor.
-            raise ValueError('In ncon.get_tcon, a trace index is listed '
-                    '!= 2 times for the same tensor.')
+            raise ValueError(
+                "In ncon.get_tcon, a trace index is listed != 2 times for the "
+                "same tensor."
+            )
     return tcon
 
 
@@ -253,38 +262,51 @@ def do_check_indices(AA, v, order, forder):
        exactly once.
     4) the dimensions of the two ends of each contracted index match.
     """
-    
-    #1)
-    if len(AA) != len(v):
-        raise ValueError(('In ncon.do_check_indices, the number of tensors %i'
-                            ' does not match the number of index lists %i')
-                           %(len(AA), len(v)))
 
-    #2)
+    # 1)
+    if len(AA) != len(v):
+        raise ValueError(
+            (
+                "In ncon.do_check_indices, the number of tensors %i"
+                " does not match the number of index lists %i"
+            )
+            % (len(AA), len(v))
+        )
+
+    # 2)
     # Create a list of lists with the shapes of each A in AA.
     shapes = list(map(lambda A: list(A.shape), AA))
-    for i,inds in enumerate(v):
+    for i, inds in enumerate(v):
         if len(inds) != len(shapes[i]):
-            raise ValueError(('In ncon.do_check_indices, len(v[%i])=%i '
-                                'does not match the numbers of indices of '
-                                'AA[%i] = %i')%(i, len(inds), i,
-                                                len(shapes[i])))
+            raise ValueError(
+                (
+                    "In ncon.do_check_indices, len(v[%i])=%i does not match "
+                    "the numbers of indices of AA[%i] = %i"
+                )
+                % (i, len(inds), i, len(shapes[i]))
+            )
 
-    #3) and 4)
+    # 3) and 4)
     # v_pairs = [[(0,0), (0,1), (0,2), ...], [(1,0), (1,1), (1,2), ...], ...]
-    v_pairs  = [[(i,j) for j in range(len(s))] for i, s in enumerate(v)]
+    v_pairs = [[(i, j) for j in range(len(s))] for i, s in enumerate(v)]
     v_pairs = sum(v_pairs, [])
     v_sum = sum(v, [])
     # For t, o in zip(v_pairs, v_sum) t is the tuple of the number of
     # the tensor and the index and o is the contraction order of that
     # index. We group these tuples by the contraction order.
-    order_groups = [[t for t, o in zip(v_pairs, v_sum) if o == e]
-                    for e in order]
+    order_groups = [
+        [t for t, o in zip(v_pairs, v_sum) if o == e] for e in order
+    ]
     forder_groups = [[1 for fo in v_sum if fo == e] for e in forder]
     for i, o in enumerate(order_groups):
         if len(o) != 2:
-            raise ValueError(('In ncon.do_check_indices, the contracted index '
-                    '%i is not featured exactly twice in v.')%order[i])
+            raise ValueError(
+                (
+                    "In ncon.do_check_indices, the contracted index %i is not "
+                    "featured exactly twice in v."
+                )
+                % order[i]
+            )
         else:
             A0, ind0 = o[0]
             A1, ind1 = o[1]
@@ -292,16 +314,22 @@ def do_check_indices(AA, v, order, forder):
                 compatible = AA[A0].compatible_indices(AA[A1], ind0, ind1)
             except AttributeError:
                 compatible = AA[A0].shape[ind0] == AA[A1].shape[ind1]
-            if not compatible: 
-                raise ValueError('In ncon.do_check_indices, for the '
-                                 'contraction index %i, the leg %i of tensor '
-                                 'number %i and the leg %i of tensor number '
-                                 '%i are not compatible.'
-                                 %(order[i], ind0, A0, ind1, A1))
+            if not compatible:
+                raise ValueError(
+                    "In ncon.do_check_indices, for the contraction index %i, "
+                    "the leg %i of tensor number %i and the leg %i of tensor "
+                    "number %i are not compatible."
+                    % (order[i], ind0, A0, ind1, A1)
+                )
     for i, fo in enumerate(forder_groups):
         if len(fo) != 1:
-            raise ValueError(('In ncon.do_check_indices, the free index '
-                    '%i is not featured exactly once in v.')%forder[i])
+            raise ValueError(
+                (
+                    "In ncon.do_check_indices, the free index %i is not "
+                    "featured exactly once in v."
+                )
+                % forder[i]
+            )
 
     # All is well if we made it here.
     return True
@@ -322,4 +350,3 @@ def con(A, B, inds):
 
 def trace(A, axis1=0, axis2=1):
     return A.trace(axis1=axis1, axis2=axis2)
-
