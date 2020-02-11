6810dd7c9d6c431fdd28a8fd54228f5fd4374afb
Markus Hauru <markus@mhauru.org>
2020-02-11 17:58:56 +0100

    Bump version number to 1.0.0.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="ncon",
-    version="0.9.0",
+    version="1.0.0",
     author="Markus Hauru",
     author_email="markus@mhauru.org",
     description="Tensor network contraction function for Python 3.",
